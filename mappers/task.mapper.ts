6052a6c5ffa478adab00cfa95ff4c779b5aa52c6
mikebalderasanchez <mikebalderasanchez@gmail.com>
2024-10-22 00:31:22 -0600

    feat: add user mappers

---
+++ mappers/task.mapper.ts
@@ -0,0 +1,13 @@
+import type { TaskEntity } from "../infrastructure/mongo/task.entity";
+import type { TaskModel } from "../models/task.model";
+
+export function TaskEntityToModel(taskEntity: TaskEntity): TaskModel {
+    return {
+        id: taskEntity.id,
+        title: taskEntity.title,
+        description: taskEntity.description,
+        status: taskEntity.status,
+        dueDate: taskEntity.dueDate,
+        createdAt: taskEntity.createdAt,
+    };
+}
