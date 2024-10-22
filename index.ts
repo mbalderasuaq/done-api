1ac3cf327c80d42846eb30f3ca27366e7f2f1740
mikebalderasanchez <mikebalderasanchez@gmail.com>
2024-10-22 00:28:46 -0600

    feat: add express

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { TaskRouter } from "./routes/task.route";
+import swaggerUi  from "swagger-ui-express";
+import swaggerSpec from "./swagger.json";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/tasks", TaskRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
